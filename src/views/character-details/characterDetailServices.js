2d33ef05718eeb8eb0fc06ba190b14e561e359b6
Rachel Hepsan <rachel.hepsan@qburst.com>
2022-12-16 13:47:45 +0530

    character page fetch  updated

--- src/views/character-details/characterDetailServices.js
+++ src/views/character-details/characterDetailServices.js
@@ -3,7 +3,7 @@ import state from "./characterDetailState.js";
 
 export const getCharacterDetail = async () => {
   const response = await axios.get(
-    `https://rickandmortyapi.com/api/character/1`
+    `https://rickandmortyapi.com/api/character/${state.results.id}`
   );
   state.results = response.data;
   console.log(state.results);
